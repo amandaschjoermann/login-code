671938b40a2040267869ad8a20f9f1eecdd854cb
Amanda Schjørmann <amandaschjoermann@gmail.com>
2018-08-01 21:47:08 +0200

    a login-page

---
+++ js/index.js
@@ -0,0 +1,9 @@
+  $('input').blur(function(event) {
+  var inputVal = this.value;
+  
+  if (inputVal) {
+    this.classList.add('value-exists');
+  } else {
+    this.classList.remove('value-exists');
+  }
+});
